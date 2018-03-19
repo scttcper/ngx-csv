9b275b56b3d0f22dcf06210631bb7a847723fcea
Scott Cooper <scttcper@gmail.com>
2018-03-18 18:45:33 -0700

    chore: fix tests

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,11 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
+import 'zone.js/dist/zone-testing';
 
 import { getTestBed } from '@angular/core/testing';
 import {
   platformBrowserDynamicTesting,
-  BrowserDynamicTestingModule
+  BrowserDynamicTestingModule,
 } from '@angular/platform-browser-dynamic/testing';
-import 'zone.js/dist/zone-testing';
-
 declare const require: any;
 
 // First, initialize the Angular testing environment.
